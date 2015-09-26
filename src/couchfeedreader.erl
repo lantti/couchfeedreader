929bc2c8fd1ba2c6a48446b417f5c80bd1733187
Antti Siponen <antti.siponen@iki.fi>
2015-09-26 23:33:32 +0200

    Change feed_sup to be started as transient to avoid restart at feed end

--- src/couchfeedreader.erl
+++ src/couchfeedreader.erl
@@ -8,7 +8,7 @@ start() -> application:ensure_all_started(couchfeedreader).
 stop() -> application:stop(couchfeedreader).
 
 follow_feed(Name, Url, Workers) ->
-  supervisor:start_child(couchfeedreader_sup, {Name, {feed_sup, start_link, [Url, Workers]}, permanent, 10000, supervisor, [feed_sup]}).
+  supervisor:start_child(couchfeedreader_sup, {Name, {feed_sup, start_link, [Url, Workers]}, transient, 10000, supervisor, [feed_sup]}).
 unfollow_feed(Name) -> 
   supervisor:terminate_child(couchfeedreader_sup, Name).
 list_feeds() -> 
