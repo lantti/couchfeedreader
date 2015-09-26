f34dcf382ff830abf711802d5d601eaefea0a54f
Antti Siponen <antti.siponen@iki.fi>
2015-09-26 22:48:45 +0200

    Shorten example_worker1 output

--- src/example_worker1.erl
+++ src/example_worker1.erl
@@ -8,23 +8,23 @@ start_link(State) -> gen_server:start_link(?MODULE, [State], []).
 
 init(State) ->
   self() ! do_work,
-  io:format("Worker got started... State: ~p~n", [State]),
+  io:format("Example worker got started.~n"),
   {ok, State}.
 
-handle_call(Request, From, State) -> io:format("Worker got call ~p from ~w... State: ~p~n", [Request, From, State]),
+handle_call(Request, From, State) -> io:format("Example worker got call ~p from ~w.~n", [Request, From]),
                                      {reply, cool, State}.
 
-handle_cast(Request, State) -> io:format("Worker got cast ~p... State: ~p~n", [Request, State]),
+handle_cast(Request, State) -> io:format("Example worker got cast ~p.~n", [Request]),
                                {noreply, State}.
 
 handle_info(do_work, State) -> 
-  io:format("Worker working... State: ~p~n", [State]),
+  io:format("Example worker working.~n"),
   {stop, normal, State};
 handle_info(Info, State) -> 
-  io:format("Worker got info ~p... State: ~p~n", [Info, State]),
+  io:format("Example worker got info ~p.~n", [Info]),
   {noreply, State}.
 
 code_change(_, State,_) -> {ok, State}.
 
-terminate(Reason, State) -> io:format("Worker got terminated with ~p... State: ~p~n", [Reason, State]).
+terminate(Reason, _) -> io:format("Example worker got terminated with ~p.~n", [Reason]).
 
